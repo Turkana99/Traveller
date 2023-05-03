fdf10fdd5179720147d67b77932a4499470cd6dc
Turkana Mammadova <mmmmadova.turkana@gmail.com>
2023-05-03 15:39:02 +0400

    Add new companents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { JumbotronComponent } from './jumbotron/jumbotron.component';
 import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { DestinationsComponent } from './destinations/destinations.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     JumbotronComponent,
-    AboutComponent
+    AboutComponent,
+    ServicesComponent,
+    DestinationsComponent
   ],
   imports: [
     BrowserModule
